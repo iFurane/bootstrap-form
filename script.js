24e5feb151edd65d8aaaae6d4a4f672fba2c1eff
Aryan Ahirwar <55043913+iFurane@users.noreply.github.com>
2020-04-05 18:42:20 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,50 @@
 var inpname = document.querySelector('input[type="text"]');
 var inpemail = document.querySelector('input[type="email"]');
+var inprollno = document.querySelector('input[id="inputRollNo"]');
+var inpstuid = document.querySelector('input[id="inputStuDep"]');
+var inpempid = document.querySelector('input[id="inputEmpID"]');
+var inpempdep = document.querySelector('input[id="inputEmpDep]');
 console.log(inpname);
 document.getElementById('submit').addEventListener('click', submitForm);
 
 function submitForm(e) {
+    var check = 0;
     var nametext = inpname.value;
     if (nametext.length <= 2) {
         document.getElementById('nameAlert').hidden = false;
-        $(".alert").fadeTo(1000, 0).slideUp(500, function() {
-            document.getElementById('nameAlert').hidden = true;
-            $(".alert").fadeTo(0, 100);
-        });
+        check++;
+    }
+    var emailtext = inpemail.value;
+    if (emailtext.indexOf('@') == -1 || emailtext.indexOf('.com') == -1) {
+        document.getElementById('emailAlert').hidden = false;
+        check++;
+    }
+    if (document.querySelector('option[value="student"]').selected == true) {
+        var rollno = inprollno.value;
+        if (rollno.length < 8) {
+            document.getElementById('rollnoAlert').hidden = false;
+            check++;
+        }
+    } else {
+        var empid = inpempid.value;
+        if (empid.length < 8) {
+            document.getElementById('empidAlert').hidden = false;
+            check++;
+        }
+        console.log(check);
+
+    }
+    $(".alert").fadeTo(1500, 0).slideUp(500, function() {
+        document.getElementById('nameAlert').hidden = true;
+        document.getElementById('emailAlert').hidden = true;
+        document.getElementById('rollnoAlert').hidden = true;
+        document.getElementById('studepAlert').hidden = true;
+        document.getElementById('empidAlert').hidden = true;
+        document.getElementById('empdepAlert').hidden = true;
+        $(".alert").fadeTo(0, 100);
+    });
+    if (check == 0) {
+        window.location.href = "submitted.html";
     }
 }
 
